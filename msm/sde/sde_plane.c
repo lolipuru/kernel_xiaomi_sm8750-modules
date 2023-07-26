CodeLinaro-mirror/femto5g_platform_vendor_opensource_display-drivers

disp: msm: sde: add debug logs for cac usecases
This change adds debug logs to check various
properties that are set during CAC usecases.

Change-Id: I7654bf04c0c3b03243e58c44bafea469552d21e1
Signed-off-by: Gaurav LNU <quic_glnu@quicinc.com>

msm/sde/sde_plane.c
@@ -2788,6 +2788,8 @@ static int _sde_plane_check_cac_mode(struct drm_plane *plane,
 		sde_plane_in_cac_fetch_mode(pstate))
 		pstate->scaler_check_state = SDE_PLANE_SCLCHECK_SCALER_V2;
 
+	SDE_DEBUG_PLANE(psde, "cac mode = %d, rec_id = %d, layout = %d\n",
+			cac_mode, rec_id, pstate->layout);
 	return ret;
 }
 
@@ -4474,6 +4476,10 @@ static void _sde_plane_set_img_size(struct sde_plane *psde,
 	pstate->src_img_rec.y = src_img_rec.y1;
 	pstate->src_img_rec.w = src_img_rec.x2 - src_img_rec.x1;
 	pstate->src_img_rec.h = src_img_rec.y2 - src_img_rec.y1;
+
+	SDE_DEBUG_PLANE(psde, "img size: {%d,%d,%d,%d}\n",
+			pstate->src_img_rec.x, pstate->src_img_rec.y,
+			pstate->src_img_rec.w, pstate->src_img_rec.h);
 }
 
 static void _sde_plane_set_ubwc_stats_roi(struct sde_plane *psde,

lolipuru/kernel_xiaomi_sm8750-modules

Signed-off-by: Sampurna Bolloju <quic_sampboll@quicinc.com>

msm/sde/sde_plane.c
@@ -2899,6 +2899,8 @@ static int _sde_plane_check_cac_mode(struct drm_plane *plane,
 		sde_plane_in_cac_fetch_mode(pstate))
 		pstate->scaler_check_state = SDE_PLANE_SCLCHECK_SCALER_V2;
 
+	SDE_DEBUG_PLANE(psde, "cac mode = %u, rec_id = %u, layout = %u\n",
+			cac_mode, rec_id, pstate->layout);
 	return ret;
 }
 
@@ -4699,6 +4701,10 @@ static void _sde_plane_set_img_size(struct sde_plane *psde,
 	pstate->src_img_rec.y = src_img_rec.y1;
 	pstate->src_img_rec.w = src_img_rec.x2 - src_img_rec.x1;
 	pstate->src_img_rec.h = src_img_rec.y2 - src_img_rec.y1;
+
+	SDE_DEBUG_PLANE(psde, "img size: {%u,%u,%u,%u}\n",
+			pstate->src_img_rec.x, pstate->src_img_rec.y,
+			pstate->src_img_rec.w, pstate->src_img_rec.h);
 }
 
 static void _sde_plane_set_ubwc_stats_roi(struct sde_plane *psde,