microsoft/surface-duo-oss-platform.vendor.opensource.display-drivers

disp: msm: dp: remove link status update check during link training
Remove link status update check during link training to align with
dp spec.

Change-Id: I525ba77133c24ccd4e0d7e5054dc51c0a38d82af
Signed-off-by: Xiaowen Wu <wxiaowen@codeaurora.org>

msm/dp/dp_ctrl.c
@@ -242,20 +242,12 @@ static int dp_ctrl_read_link_status(struct dp_ctrl_private *ctrl,
 					u8 *link_status)
 {
 	int ret = 0, len;
-	u32 const offset = DP_LANE_ALIGN_STATUS_UPDATED - DP_LANE0_1_STATUS;
-	u32 link_status_read_max_retries = 100;
 
-	while (--link_status_read_max_retries) {
-		len = drm_dp_dpcd_read_link_status(ctrl->aux->drm_aux,
-			link_status);
-		if (len != DP_LINK_STATUS_SIZE) {
-			pr_err("DP link status read failed, err: %d\n", len);
-			ret = len;
-			break;
-		}
-
-		if (!(link_status[offset] & DP_LINK_STATUS_UPDATED))
-			break;
+	len = drm_dp_dpcd_read_link_status(ctrl->aux->drm_aux,
+		link_status);
+	if (len != DP_LINK_STATUS_SIZE) {
+		pr_err("DP link status read failed, err: %d\n", len);
+		ret = len;
 	}
 
 	return ret;

lolipuru/kernel_xiaomi_sm8750-modules

Signed-off-by: Soutrik Mukhopadhyay <quic_mukhopad@quicinc.com>
Signed-off-by: Kashish Jain <quic_kashjain@quicinc.com>

msm/dp/dp_ctrl.c
@@ -267,20 +267,11 @@ static int dp_ctrl_read_link_status(struct dp_ctrl_private *ctrl,
 					u8 *link_status)
 {
 	int ret = 0, len;
-	u32 const offset = DP_LANE_ALIGN_STATUS_UPDATED - DP_LANE0_1_STATUS;
-	u32 link_status_read_max_retries = 100;
 
-	while (--link_status_read_max_retries) {
-		len = drm_dp_dpcd_read_link_status(ctrl->aux->drm_aux,
-			link_status);
-		if (len != DP_LINK_STATUS_SIZE) {
-			DP_ERR("DP link status read failed, err: %d\n", len);
-			ret = len;
-			break;
-		}
-
-		if (!(link_status[offset] & DP_LINK_STATUS_UPDATED))
-			break;
+	len = drm_dp_dpcd_read_link_status(ctrl->aux->drm_aux, link_status);
+	if (len != DP_LINK_STATUS_SIZE) {
+		DP_ERR("DP link status read failed, err: %d\n", len);
+		ret = len;
 	}
 
 	return ret;