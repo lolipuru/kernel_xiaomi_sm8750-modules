847a88d596f9a2885373d7e82cb055a3fef9a6bf
Ekansh Gupta <quic_ekangupt@quicinc.com>
2024-02-23 12:03:25 +0530

    dsp-kernel: Add fastrpc events tracing
    Add changes to trace fastrpc events. Certain events like map, unmap,
    buffer alloc, free, message send, receive etc. will be traced with
    this change. This would help capture events in ftrace and QTF tracer
    tool.
    
    Change-Id: I25cd23a505910bb9265cef09c068a69bdc8f596f
    Signed-off-by: Ekansh Gupta <quic_ekangupt@quicinc.com>
    Signed-off-by: Anirudh Raghavendra <quic_araghave@quicinc.com>

--- dsp/fastrpc_shared.h
+++ dsp/fastrpc_shared.h
@@ -123,6 +123,7 @@
 #define FASTRPC_RMID_INIT_CREATE_STATIC	8
 #define FASTRPC_RMID_INIT_MEM_MAP      10
 #define FASTRPC_RMID_INIT_MEM_UNMAP    11
+#define FASTRPC_RMID_INIT_MAX (20)
 
 #define miscdev_to_fdevice(d) container_of(d, struct fastrpc_device_node, miscdev)
 
