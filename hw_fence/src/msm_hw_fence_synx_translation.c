549a2cce75ed96df19bc985c403842f7ea996f5e
Grace An <quic_gracan@quicinc.com>
2024-03-07 17:32:52 -0800

    mm-drivers: hw_fence: add mask to handle comparison for synx_wait
    The handle received through client rx queue may or may not have handle
    flag bit set. Thus, add mask to handle comparison when an apps loopback
    client reads the entry in client rx queue.
    
    Change-Id: Ibce45d3d1f2de73fe91b6ce2c2ce8c88ab37570c
    Signed-off-by: Grace An <quic_gracan@quicinc.com>

--- hw_fence/src/msm_hw_fence_synx_translation.c
+++ hw_fence/src/msm_hw_fence_synx_translation.c
@@ -249,7 +249,7 @@ static int synx_hwfence_wait(struct synx_session *session, u32 h_synx, u64 timeo
 	if (session->type >= SYNX_CLIENT_HW_FENCE_TEST_CTX0
 			&& session->type <= SYNX_CLIENT_HW_FENCE_TEST_CTX0 + MAX_SUPPORTED_TEST)
 		ret = hw_fence_debug_wait_val(hw_fence_drv_data, session->client, NULL, h_synx,
-			timeout_ms, &error);
+			HW_FENCE_HANDLE_INDEX_MASK, timeout_ms, &error);
 #endif /* CONFIG_DEBUG_FS */
 
 	if (ret) {
