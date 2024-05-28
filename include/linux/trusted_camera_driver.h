d2569d01d184f7020a3110550cb4c60a2ac037e8
Rajat Singh Deepak <quic_rajatsin@quicinc.com>
2024-05-28 23:49:04 +0530

    securemsm-kernel: Add support for port configuration
    - Expose dynamicConfigurePorts to camera kernel for port
      configurations.
    - Added enums instead of mask for port config.
    
    Build and Compilation verified.
    Scipp test validated the super API on Matrix with IFE ,IFE LITE
    and IPE configurations.
    E2E also validated by QIPL Camera team.
    LOGS: \\lab0756\dropbox\for_rajat\Matrix_DCPV2_logs.
    
    Change-Id: I9b2c1477316fa57b0f5cdf3412485a6aed95d6cd
    Signed-off-by: Rajat Singh Deepak <quic_rajatsin@quicinc.com>

--- include/linux/trusted_camera_driver.h
+++ include/linux/trusted_camera_driver.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only WITH Linux-syscall-note */
 /*
- * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include "smcinvoke_object.h"
@@ -33,7 +33,7 @@
  * @reserved:           Reserved bit
  */
 
-typedef struct {
+struct tc_driver_sensor_info {
 	uint32_t version;
 	uint32_t protect;
 	uint32_t csid_hw_idx_mask;
@@ -41,7 +41,7 @@ typedef struct {
 	uint64_t vc_mask;
 	uint64_t phy_lane_sel_mask;
 	uint64_t reserved;
-} ITCDriverSensorInfo;
+};
 
 /**
  * Struct containing values for configuration of ports
@@ -59,7 +59,7 @@ typedef struct {
  *                      defined by 'num_ports'
  */
 
-struct PortInfo {
+struct port_info {
 	uint32_t hw_type;
 	uint32_t hw_id_mask;
 	uint32_t protect;
@@ -156,13 +156,13 @@ enum ipe_output_port_id {
 #define ITRUSTEDCAMERADRIVER_OP_DYNAMICCONFIGUREPORTSV2 4
 
 static inline int32_t
-ITrustedCameraDriver_release(struct Object self)
+trusted_camera_driver_release(struct Object self)
 {
 	return Object_invoke(self, Object_OP_release, 0, 0);
 }
 
 static inline int32_t
-ITrustedCameraDriver_retain(struct Object self)
+trusted_camera_driver_retain(struct Object self)
 {
 	return Object_invoke(self, Object_OP_retain, 0, 0);
 }
@@ -179,12 +179,12 @@ ITrustedCameraDriver_retain(struct Object self)
  */
 
 static inline int32_t
-ITrustedCameraDriver_dynamicProtectSensor(struct Object self,
-		const ITCDriverSensorInfo *phy_info_ptr)
+trusted_camera_driver_dynamic_protect_sensor(struct Object self,
+		const struct tc_driver_sensor_info *phy_info_ptr)
 {
 	union ObjectArg a[1] = {{{0, 0}}};
 
-	a[0].bi = (struct ObjectBufIn) { phy_info_ptr, sizeof(ITCDriverSensorInfo) };
+	a[0].bi = (struct ObjectBufIn) { phy_info_ptr, sizeof(struct tc_driver_sensor_info) };
 
 	return Object_invoke(self, ITRUSTEDCAMERADRIVER_OP_DYNAMICPROTECTSENSOR, a,
 			ObjectCounts_pack(1, 0, 0, 0));
@@ -201,7 +201,7 @@ ITrustedCameraDriver_dynamicProtectSensor(struct Object self,
  */
 
 static inline int32_t
-ITrustedCameraDriver_getVersion(struct Object self, uint32_t *arch_ver_ptr,
+trusted_camera_driver_get_version(struct Object self, uint32_t *arch_ver_ptr,
 		uint32_t *max_ver_ptr, uint32_t *min_ver_ptr)
 {
 	union ObjectArg a[1] = {{{0, 0}}};
@@ -238,7 +238,7 @@ ITrustedCameraDriver_getVersion(struct Object self, uint32_t *arch_ver_ptr,
  */
 
 static inline int32_t
-ITrustedCameraDriver_dynamicConfigureFDPort(struct Object self, uint32_t protect)
+trusted_camera_driver_dynamic_configure_fd_port(struct Object self, uint32_t protect)
 {
 	union ObjectArg a[1] = {{{0, 0}}};
 
@@ -252,8 +252,8 @@ ITrustedCameraDriver_dynamicConfigureFDPort(struct Object self, uint32_t protect
  * Description: Dynamic configuration for secure and non-secure port on a given HWTYPE
  *
  * In:          this - ITrustedCameraDriver object
- * In:          port_info_ptr - structure containing the PortInfo
- * In:          port_info_len - length of PortInfo structure
+ * In:          port_info_ptr - structure containing the port_info
+ * In:          port_info_len - length of port_info structure
  * Out:         void
  * Return:      Object_OK on success
  *              Object_ERROR on failure
@@ -262,12 +262,12 @@ ITrustedCameraDriver_dynamicConfigureFDPort(struct Object self, uint32_t protect
  */
 
 static inline int32_t
-ITrustedCameraDriver_dynamicConfigurePortsV2(struct Object self,
-		const struct PortInfo *port_info_ptr, size_t port_info_len)
+trusted_camera_driver_dynamic_configure_ports_v2(struct Object self,
+		const struct port_info *port_info_ptr, size_t port_info_len)
 {
 	union ObjectArg a[1] = {{{0, 0}}};
 
-	a[0].bi = (struct ObjectBufIn) { port_info_ptr, port_info_len * sizeof(struct PortInfo) };
+	a[0].bi = (struct ObjectBufIn) { port_info_ptr, port_info_len * sizeof(struct port_info) };
 
 	return Object_invoke(self, ITRUSTEDCAMERADRIVER_OP_DYNAMICCONFIGUREPORTSV2, a,
 			ObjectCounts_pack(1, 0, 0, 0));
