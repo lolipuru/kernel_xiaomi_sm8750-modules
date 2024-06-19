100409931641f8e34aa18b8ad28f4d72973aa801
Bruce Levy <quic_blevy@quicinc.com>
2024-06-18 22:33:43 -0700

    Revert "msm: eva: restore struct size check for dsp msg"
    This reverts commit 19eca6b941f757554974157c54a978d31d14e3b0.
    
    Signed-off-by: Bruce Levy <quic_blevy@quicinc.com>
    Change-Id: I4d55e446acfae1bb26873136087a2210c07be37a

--- msm/eva/msm_cvp_dsp.c
+++ msm/eva/msm_cvp_dsp.c
@@ -479,7 +479,8 @@ static int cvp_dsp_rpmsg_callback(struct rpmsg_device *rpdev,
 			goto exit;
 		}
 	} else if (rsp->type < CVP_DSP_MAX_CMD
-			&& len == sizeof(struct cvp_dsp2cpu_cmd)
+			/* Restore below size check after dsp change released */
+			/*  && len == sizeof(struct cvp_dsp2cpu_cmd) */
 			) {
 		if (me->pending_dsp2cpu_cmd.type != CVP_INVALID_RPMSG_TYPE) {
 			dprintk(CVP_ERR,
