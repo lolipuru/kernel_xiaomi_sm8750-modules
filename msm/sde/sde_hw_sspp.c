10e7d9698b486662badc9d3c7ce1669eff7e65e7
Lei Chen <quic_chenlei@quicinc.com>
2024-08-05 18:13:48 +0800

    disp: msm: sde: disable const-alpha for RGB_10BIT and FP16 formats
    Disable constant alpha feature for RGB_10BIT and FP16 formats to
    fix corruption issues.
    
    Change-Id: I8682465f99b98ac8f1378dd61677b04a2ba2397c
    Signed-off-by: Lei Chen <quic_chenlei@quicinc.com>

--- msm/sde/sde_hw_sspp.c
+++ msm/sde/sde_hw_sspp.c
@@ -346,7 +346,9 @@ static void sde_hw_sspp_setup_ubwc(struct sde_hw_pipe *ctx, struct sde_hw_blk_re
 		ubwc_ctrl_off = SSPP_UBWC_STATIC_CTRL_REC1;
 
 	if (SDE_HW_MAJOR(ctx->catalog->ubwc_rev) >= SDE_HW_MAJOR(SDE_HW_UBWC_VER_50)) {
-		ctrl_val |= SDE_FORMAT_IS_YUV(fmt) ? 0 : (BIT(31) | BIT(30));
+		if (!SDE_FORMAT_IS_YUV(fmt))
+			ctrl_val |= (SDE_FORMAT_IS_DX(fmt) || SDE_FORMAT_IS_FP16(fmt)) ?
+					BIT(30) : (BIT(31) | BIT(30));
 		ctrl_val |= SDE_FORMAT_IS_UBWC_LOSSY_2_1(fmt) ? (0x3 << 16) : 0;
 		ctrl_val |= SDE_FORMAT_IS_UBWC_LOSSY_8_5(fmt) ? BIT(16) : 0;
 		SDE_REG_WRITE(c, ubwc_ctrl_off, ctrl_val);
