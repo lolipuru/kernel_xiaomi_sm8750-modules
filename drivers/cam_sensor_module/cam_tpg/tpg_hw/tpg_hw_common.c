9287f7e0bd763d25918a6e5e79c7cd5c870f3481
Wenmeng Liu <quic_wenmliu@quicinc.com>
2024-08-06 17:38:16 +0800

    msm: camera: tpg: Add TPG write settings offset check
    offset boundary checking is added to solve the OOB
    problem when invalid offset is passed in.
    
    CRs-Fixed: 3885358
    Change-Id: I4855e32221f4a3a8fbe482ad5e27b32d8c1738c3
    Signed-off-by: Wenmeng Liu <quic_wenmliu@quicinc.com>

--- drivers/cam_sensor_module/cam_tpg/tpg_hw/tpg_hw_common.c
+++ drivers/cam_sensor_module/cam_tpg/tpg_hw/tpg_hw_common.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022,2024 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include "tpg_hw_common.h"
@@ -10,6 +10,8 @@ int tpg_hw_write_settings(struct tpg_hw *hw,
 {
 		struct cam_hw_soc_info *soc_info = NULL;
 		int setting_index;
+		phys_addr_t size;
+		uint32_t tpg_reg_offset;
 
 		if (!hw || !config || !settings) {
 			CAM_ERR(CAM_TPG, "invalid params");
@@ -17,9 +19,16 @@ int tpg_hw_write_settings(struct tpg_hw *hw,
 		}
 
 		soc_info = hw->soc_info;
+		size = resource_size(soc_info->mem_block[0]);
+
 		for (setting_index = 0; setting_index < config->active_count; setting_index++) {
+			tpg_reg_offset = settings->reg_offset;
+			if (tpg_reg_offset >= size) {
+				CAM_ERR(CAM_TPG, "settings reg_offset error");
+				return -EINVAL;
+			}
 			cam_io_w_mb(settings->reg_value, soc_info->reg_map[0].mem_base +
-						settings->reg_offset);
+						tpg_reg_offset);
 			settings++;
 		}
 
