6a6ab5f74606d6bf74a9db6451130deb3d97ce3c
Prateek Patil <quic_pratpati@quicinc.com>
2024-12-19 17:52:09 +0530

    icnss2: Defer cnss probing until SMMU probe is complete
    This change returns EPROBE_DEFER when priv->iommu_domain
    is null so that cnss probe defers until smmu probe is
    completed.
    
    Change-Id: I8db857fc237ea7533142aeb46bf36ffd8b662748
    CRs-Fixed: 4007355

--- icnss2/main.c
+++ icnss2/main.c
@@ -5584,6 +5584,9 @@ static int icnss_smmu_dt_parse(struct icnss_priv *priv)
 		priv->iommu_domain =
 			iommu_get_domain_for_dev(&pdev->dev);
 
+		if (!priv->iommu_domain)
+			return -EPROBE_DEFER;
+
 		ret = of_property_read_string(of_node, "qcom,iommu-dma",
 					      &iommu_dma_type);
 		if (!ret && !strcmp("fastmap", iommu_dma_type)) {
