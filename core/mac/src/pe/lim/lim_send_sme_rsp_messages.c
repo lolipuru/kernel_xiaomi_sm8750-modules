474a2ee43a4884587472593c80e991c34791b76b
Aasir Rasheed <quic_arasheed@quicinc.com>
2025-01-03 10:29:38 +0530

    qcacld-3.0: Revert “qcacld-3.0: avoid assert after upgrade bw by CSA"
    This reverts Change-Id: Ie9dc47420f22a8f0c1aa4234d2c2fb2b4b8ed431
    
    The reverted commit is blocking bandwidth upgrades via
    CSA and does not handle EHT mode properly.
    The appropriate fix should address MCS handling rather than
    limiting bandwidth upgrades.
    
    CRs-Fixed: 4011166
    Change-Id: Ia197cf0ecb119b3ac1ce6e4446ff9553d4c3b1f2

--- core/mac/src/pe/lim/lim_send_sme_rsp_messages.c
+++ core/mac/src/pe/lim/lim_send_sme_rsp_messages.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2012-2021 The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2025 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -2087,20 +2087,6 @@ static void lim_csa_update_channel_width_for_dot11mode(
 	}
 }
 
-static void lim_csa_update_bw_for_assoc(uint8_t vdev_id,
-					struct csa_offload_params *csa_params)
-{
-	enum phy_ch_width max_ch_width;
-
-	max_ch_width = wma_get_assoc_bw(vdev_id);
-	if (csa_params->new_ch_width > max_ch_width) {
-		pe_debug("Downgrade bw from %d to assoc bw %d",
-			 csa_params->new_ch_width, max_ch_width);
-		max_ch_width = wma_get_assoc_bw(vdev_id);
-		csa_params->new_ch_width = max_ch_width;
-	}
-}
-
 void lim_handle_sta_csa_param(struct mac_context *mac_ctx,
 			      struct csa_offload_params *csa_params,
 			      bool send_status)
@@ -2155,9 +2141,6 @@ void lim_handle_sta_csa_param(struct mac_context *mac_ctx,
 	lim_ch_switch = &session_entry->gLimChannelSwitch;
 
 	lim_csa_update_channel_width_for_dot11mode(session_entry, csa_params);
-
-	lim_csa_update_bw_for_assoc(session_entry->vdev_id, csa_params);
-
 	ch_params.ch_width = csa_params->new_ch_width;
 
 	if (IS_DOT11_MODE_EHT(session_entry->dot11mode))
