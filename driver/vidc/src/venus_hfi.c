d730a5f54835c5b55d222ef17506054e0a1a4521
Govindaraj Rajagopal <quic_grajagop@quicinc.com>
2025-01-27 17:29:07 +0530

    video: driver: fix close cmd timeout issue
    In stability testing, OPEN & CLOSE cmds were called frequently
    so sometimes newly opened inst is not part of instances array
    and response_handler is still processing previous session
    packets and close response also might come on same iteration.
    Since that inst is not available in local instances cache
    reverse thread treats that inst as invalid and skips close
    response handling and i.e resulting into CLOSE cmd timeout
    issue.
    
    Added change to search in &core->instances list, incase inst
    is not found in local instances cache.
    
    Change-Id: I229f70e0da3220278fa3a38a3c537ff0d4462335
    Signed-off-by: Govindaraj Rajagopal <quic_grajagop@quicinc.com>

--- driver/vidc/src/venus_hfi.c
+++ driver/vidc/src/venus_hfi.c
@@ -617,7 +617,7 @@ void __unload_fw(struct msm_vidc_core *core)
 	d_vpr_h("%s unloaded video firmware\n", __func__);
 }
 
-static inline struct msm_vidc_inst *get_inst(
+static inline struct msm_vidc_inst *get_inst_cached(
 	struct msm_vidc_inst *const *const instances, const s32 count, u32 session_id)
 {
 	struct msm_vidc_inst *inst = NULL;
@@ -657,15 +657,27 @@ static int __process_msg_q(struct msm_vidc_core *core,
 		if (!hdr->session_id) {
 			rc = handle_system_response(core, hdr);
 		} else {
-			inst = get_inst(instances, num_instances, hdr->session_id);
+			bool local_inst = false;
+
+			inst = get_inst_cached(instances, num_instances, hdr->session_id);
 			if (!inst) {
-				d_vpr_e("%s: Invalid inst - %#x\n", __func__, hdr->session_id);
-				rc = -EINVAL;
-				goto error;
+				d_vpr_l("%s: inst not found in cache - %#x\n",
+					__func__, hdr->session_id);
+				inst = get_inst(core, hdr->session_id);
+				if (!inst) {
+					d_vpr_e("%s: Invalid inst - %#x\n",
+						__func__, hdr->session_id);
+					rc = -EINVAL;
+					goto error;
+				}
+				local_inst = true;
 			}
 			inst_lock(inst, __func__);
 			rc = handle_session_response(inst, hdr);
 			inst_unlock(inst, __func__);
+
+			if (local_inst)
+				put_inst(inst);
 		}
 error:
 		if (rc)
