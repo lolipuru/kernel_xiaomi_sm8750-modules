9b74a0722fe9476cdec58c536093dbb63630bee4
Amit Mehta <quic_amitmeht@quicinc.com>
2025-02-18 14:27:58 +0530

    qcacld-3.0: Skip FISA aggregation in case of MSDU len mismatch
    Currently there is no sanity done for MSDU length between rx desc
    and rx tlv, In case if msdu length in rx pkt tlv or rx desc is not
    correct it will result in wrong nbuf length update. Which can result
    in Kernel panic in FISA aggregation case.
    
    So to fix the issue skip FISA aggregation if there is mismatch
    in MSDU length between rx desc and rx pkt tlv.
    
    CRs-Fixed: 4059433
    Change-Id: I9e72808f566db4d5992f0189130fe42f0c64c0a6

--- components/dp/core/src/wlan_dp_fisa_rx.c
+++ components/dp/core/src/wlan_dp_fisa_rx.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2021-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2021-2025 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -2184,6 +2184,7 @@ static void dp_rx_fisa_flush_flow(struct dp_vdev *vdev,
  * @hal_aggr_count: current aggregate count from RX PKT TLV
  * @hal_cumulative_ip_len: current cumulative ip length from RX PKT TLV
  * @rx_tlv_hdr: current msdu RX PKT TLV
+ * @nbuf: incoming nbuf
  *
  * Return: true - current flow aggregation should stop,
  *	   false - continue to aggregate.
@@ -2192,7 +2193,7 @@ static bool dp_fisa_aggregation_should_stop(
 				struct dp_fisa_rx_sw_ft *fisa_flow,
 				uint32_t hal_aggr_count,
 				uint16_t hal_cumulative_ip_len,
-				uint8_t *rx_tlv_hdr)
+				uint8_t *rx_tlv_hdr, qdf_nbuf_t nbuf)
 {
 	uint32_t msdu_len =
 		hal_rx_msdu_start_msdu_len_get(fisa_flow->dp_ctx->hal_soc,
@@ -2236,7 +2237,8 @@ static bool dp_fisa_aggregation_should_stop(
 	    hal_cumulative_ip_len <= fisa_flow->hal_cumultive_ip_len ||
 	    cumulative_ip_len_delta > FISA_MAX_SINGLE_CUMULATIVE_IP_LEN ||
 	    (fisa_flow->last_hal_aggr_count + 1) != hal_aggr_count ||
-	    cumulative_ip_len_delta != (msdu_len - l2_l3_hdr_len))
+	    cumulative_ip_len_delta != (msdu_len - l2_l3_hdr_len) ||
+	    msdu_len != QDF_NBUF_CB_RX_PKT_LEN(nbuf))
 		return true;
 
 	return false;
@@ -2338,7 +2340,7 @@ static int dp_add_nbuf_to_fisa_flow(struct dp_rx_fst *fisa_hdl,
 						fisa_flow,
 						hal_aggr_count,
 						hal_cumulative_ip_len,
-						rx_tlv_hdr))) {
+						rx_tlv_hdr, nbuf))) {
 			qdf_assert(0);
 			fisa_flow->do_not_aggregate = true;
 			/*
@@ -2351,7 +2353,7 @@ static int dp_add_nbuf_to_fisa_flow(struct dp_rx_fst *fisa_hdl,
 						fisa_flow,
 						hal_aggr_count,
 						hal_cumulative_ip_len,
-						rx_tlv_hdr))) {
+						rx_tlv_hdr, nbuf))) {
 		qdf_assert(0);
 		/* Either HW cumulative ip length is wrong, or packet is missed
 		 * Flush the flow and do not aggregate until next start new
