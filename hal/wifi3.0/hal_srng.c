09d08981baa260dae6d056a10710c7103be6fa51
Asutosh Mohapatra <asutmoha@qti.qualcomm.com>
2025-05-01 00:07:08 -0700

    qcacmn: Fix compilation issue for WCN7760
    Add a break statement in switch case in
    hal_target_based_configure for WCN7760(QCC2072) to
    avoid compilation failure
    
    Change-Id: I80bc9d7b93b8a927a970e494bdbe9c0550ead6f2
    CRs-Fixed: 4116254

--- hal/wifi3.0/hal_srng.c
+++ hal/wifi3.0/hal_srng.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2016-2021 The Linux Foundation. All rights reserved.
- * Copyright (c) 2021-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -466,6 +466,7 @@ static void hal_target_based_configure(struct hal_soc *hal)
 	case TARGET_TYPE_QCC2072:
 		hal->use_register_windowing = true;
 		hal_qcc2072_attach(hal);
+		break;
 #endif
 #ifdef INCLUDE_HAL_KIWI
 	case TARGET_TYPE_KIWI:
